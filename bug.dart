d7fb76779f07fb9ca63b1c0b6a6a944403828f43
BugHunter-X <askzenai@gmail.com>
2025-01-06 22:44:04 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,5 @@
+```dart
+List<int> list = [1, 2, 3, 4, 5];
+int index = 6;
+int value = list[index]; // This will throw an out of bounds error because the index is greater than the list length.
+```
